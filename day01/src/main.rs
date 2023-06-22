37df6a6096198f6ddf1c43550fd9a2369037477f
Max Torf <torfstack@gmail.com>
2023-06-23 00:01:27 +0200

    added: day01 solution 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,26 +2,47 @@ use std::fs;
 
 fn main() {
     let lines = read_input();
-    let mut current = 99_999;
-    let mut count = 0;
+    solution1(&lines);
+    solution2(&lines);
+}
+
+fn solution1(lines: &Vec<u32>) {
+    let mut current: u32 = 99_999;
+    let mut count: u32 = 0;
     lines[..lines.len()-1]
         .iter()
-        .map(|line| line.parse::<u32>().unwrap())
-        .for_each(|n| {
+        .for_each(|&n| {
             if n > current {
                 count = count+1;
             }
-            current = n
+            current = n;
         });
 
-    println!("{}", count)
+    println!("Solution 1: {}", count)
 }
 
-fn read_input() -> Vec<String> {
+fn solution2(lines: &Vec<u32>) {
+    let mut current: u32 = 99_999;
+    let mut count: u32 = 0;
+
+    for i in 0..lines.len()-2 {
+        let pivot = lines[i] + lines[i+1] + lines[i+2];
+        if pivot > current {
+            count = count + 1;
+        }
+        current = pivot;
+    }
+
+    println!("Solution 2: {}", count)
+}
+
+fn read_input() -> Vec<u32> {
     let read = fs::read_to_string("input").unwrap();
-    println!("{}", read);
     return read
         .split("\n")
         .map(|l| l.to_string())
+        .map(|s| s.parse::<u32>())
+        .filter(|p| p.is_ok())
+        .map(|p| p.unwrap())
         .collect()
 }
