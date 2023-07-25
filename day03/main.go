c238baac2755a512072ca1f4c46c515a4a5dc836
Max Torf <torfstack@gmail.com>
2023-07-25 23:13:26 +0200

    gofmt day03

--- day03/main.go
+++ day03/main.go
@@ -9,48 +9,48 @@ import (
 )
 
 func main() {
-    solve("example")
-    solve("input")
+	solve("example")
+	solve("input")
 }
 
 func solve(name string) {
-    input := readInFile(name)
-    length := len(input[0])
-    count := len(input)
+	input := readInFile(name)
+	length := len(input[0])
+	count := len(input)
 
-    gamma := []int{}
-    for i := 0; i < length; i++ {
-        gamma = append(gamma, 0)
-    }
+	gamma := []int{}
+	for i := 0; i < length; i++ {
+		gamma = append(gamma, 0)
+	}
 
-    for _, l := range input {
-        if l == "" {
-            continue
-        }
-        for i := 0; i < length; i++ {
-            if string(l[i]) == "1" {
-                gamma[i] = gamma[i] + 1
-            }
-        }
-    }
+	for _, l := range input {
+		if l == "" {
+			continue
+		}
+		for i := 0; i < length; i++ {
+			if string(l[i]) == "1" {
+				gamma[i] = gamma[i] + 1
+			}
+		}
+	}
 
-    var buf = bytes.Buffer{}
-    for i := 0; i < length; i++ {
-        if gamma[i] >= count/2.0 {
-            buf.WriteString("1")
-        } else {
-            buf.WriteString("0")
-        }
-    }
-    gammaString := buf.String()
-    gammaUint, _ := strconv.ParseUint(gammaString, 2, 32)
-    pivot := math.Pow(2, float64(length)) - 1
-    println(gammaUint * (uint64(pivot) - gammaUint))
+	var buf = bytes.Buffer{}
+	for i := 0; i < length; i++ {
+		if gamma[i] >= count/2.0 {
+			buf.WriteString("1")
+		} else {
+			buf.WriteString("0")
+		}
+	}
+	gammaString := buf.String()
+	gammaUint, _ := strconv.ParseUint(gammaString, 2, 32)
+	pivot := math.Pow(2, float64(length)) - 1
+	println(gammaUint * (uint64(pivot) - gammaUint))
 }
 
 func readInFile(name string) []string {
-    f, _:= os.ReadFile(name)
-    fStr := string(f)
-    fSplit := strings.Split(fStr, "\n")
-    return fSplit
+	f, _ := os.ReadFile(name)
+	fStr := string(f)
+	fSplit := strings.Split(fStr, "\n")
+	return fSplit
 }
